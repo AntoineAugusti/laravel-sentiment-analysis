8f1300bc46ffa6b741550a6b4b259c5f03988dd1
Antoine Augusti <antoine@augusti.fr>
2015-02-08 09:53:35 +0100

    Update for Laravel 5 and move to PSR-4

---
+++ src/LaravelSentimentAnalysisServiceProvider.php
@@ -0,0 +1,18 @@
+<?php namespace Antoineaugusti\LaravelSentimentAnalysis;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSentimentAnalysisServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['sentimentanalysis'] = $this->app->share(function($app) {
+			return new SentimentAnalysis;
+		});
+	}
+}
