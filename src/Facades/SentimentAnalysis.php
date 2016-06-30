e090b0707f0e62ba1fbb334043427b82559ab223
Antoine Augusti <antoine.augusti@gmail.com>
2016-06-30 08:40:29 -0400

    Applied fixes from StyleCI

--- src/Facades/SentimentAnalysis.php
+++ src/Facades/SentimentAnalysis.php
@@ -1,14 +1,18 @@
-<?php namespace Antoineaugusti\LaravelSentimentAnalysis\Facades;
+<?php
+
+namespace Antoineaugusti\LaravelSentimentAnalysis\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 class SentimentAnalysis extends Facade
 {
-	/**
-	 * Get the registered name of the component.
-	 *
-	 * @return string
-	 */
-	protected static function getFacadeAccessor() { return 'sentimentanalysis'; }
-
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'sentimentanalysis';
+    }
 }
