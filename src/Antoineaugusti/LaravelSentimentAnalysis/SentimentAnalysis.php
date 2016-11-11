0e3f8a5945f40e8e74aa6ffff820d7e8c420a546
Antoine Augusti <antoine.augusti@gmail.com>
2016-11-11 20:59:09 +0000

    Applied fixes from StyleCI

--- src/Antoineaugusti/LaravelSentimentAnalysis/SentimentAnalysis.php
+++ src/Antoineaugusti/LaravelSentimentAnalysis/SentimentAnalysis.php
@@ -1,101 +1,118 @@
-<?php namespace Antoineaugusti\LaravelSentimentAnalysis;
+<?php
+
+namespace Antoineaugusti\LaravelSentimentAnalysis;
 
 use PHPInsight\Sentiment;
 
-class SentimentAnalysis {
+class SentimentAnalysis
+{
+    private $sentiment;
+    const NEGATIVE = 'negative';
+    const NEUTRAL = 'neutral';
+    const POSITIVE = 'positive';
 
-	private $sentiment;
-	const NEGATIVE = 'negative';
-	const NEUTRAL  = 'neutral';
-	const POSITIVE = 'positive';
+    public function __construct()
+    {
+        $this->sentiment = new Sentiment();
+    }
 
-	public function __construct()
-	{	
-		$this->sentiment = new Sentiment();
-	}
+    /**
+     * Get the sentiment of a phrase.
+     *
+     * @param string $string The given sentence
+     *
+     * @return string Possible values: negative|neutral|positive
+     */
+    public function decision($string)
+    {
+        // Do not call functions so that we'll compute only one time
+        $dominantClass = $this->sentiment->categorise($string);
 
-	/**
-	 * Get the sentiment of a phrase
-	 * @param  string $string The given sentence
-	 * @return string Possible values: negative|neutral|positive
-	 */
-	public function decision($string)
-	{
-		// Do not call functions so that we'll compute only one time
-		$dominantClass = $this->sentiment->categorise($string);
-		
-		switch ($dominantClass) {
-			case 'neg':
-				return self::NEGATIVE;
-				break;
+        switch ($dominantClass) {
+            case 'neg':
+                return self::NEGATIVE;
+                break;
 
-			case 'neu':
-				return self::NEUTRAL;
-				break;
+            case 'neu':
+                return self::NEUTRAL;
+                break;
 
-			case 'pos':
-				return self::POSITIVE;
-				break;
-		}
-	}
+            case 'pos':
+                return self::POSITIVE;
+                break;
+        }
+    }
 
-	/**
-	 * Get scores for each decision
-	 * @param  string $string The original string
-	 * @return array  An array containing keys 'negative', 'neutral' and 'positive' with a float. The closer to 1, the better
-	 * @example ['negative' => 0.5, 'neutral' => 0.25, 'positive' => 0.25]
-	 */
-	public function scores($string)
-	{
-		$scores = $this->sentiment->score($string);
-		$array = array();
+    /**
+     * Get scores for each decision.
+     *
+     * @param string $string The original string
+     *
+     * @return array An array containing keys 'negative', 'neutral' and 'positive' with a float. The closer to 1, the better
+     *
+     * @example ['negative' => 0.5, 'neutral' => 0.25, 'positive' => 0.25]
+     */
+    public function scores($string)
+    {
+        $scores = $this->sentiment->score($string);
+        $array = [];
 
-		// The original keys are 'neg' / 'neu' / 'pos'
-		// We will remap to 'negative' / 'neutral' / 'positive' and round with 2 digits
-		foreach ([self::NEGATIVE, self::NEUTRAL, self::POSITIVE] as $value)
-			$array[$value] = round($scores[substr($value, 0, 3)], 2);
-		
-		return $array;
-	}
+        // The original keys are 'neg' / 'neu' / 'pos'
+        // We will remap to 'negative' / 'neutral' / 'positive' and round with 2 digits
+        foreach ([self::NEGATIVE, self::NEUTRAL, self::POSITIVE] as $value) {
+            $array[$value] = round($scores[substr($value, 0, 3)], 2);
+        }
 
-	/**
-	 * Get the confidence of a decision for a result. The closer to 1, the better
-	 * @param  string $string The given sentence
-	 * @return float The confidence of a decision for a result. The close to 1, the better
-	 */
-	public function score($string)
-	{
-		$scores = $this->scores($string);
-		return max($scores);
-	}
+        return $array;
+    }
 
-	/**
-	 * Tells if a sentence is positive
-	 * @param  string $string The given sentence
-	 * @return boolean
-	 */
-	public function isPositive($string)
-	{
-		return $this->decision($string) == self::POSITIVE;
-	}
+    /**
+     * Get the confidence of a decision for a result. The closer to 1, the better.
+     *
+     * @param string $string The given sentence
+     *
+     * @return float The confidence of a decision for a result. The close to 1, the better
+     */
+    public function score($string)
+    {
+        $scores = $this->scores($string);
 
-	/**
-	 * Tells if a sentence is negative
-	 * @param  string $string The given sentence
-	 * @return boolean
-	 */
-	public function isNegative($string)
-	{
-		return $this->decision($string) == self::NEGATIVE;
-	}
+        return max($scores);
+    }
 
-	/**
-	 * Tells if a sentence is neutral
-	 * @param  string $string The given sentence
-	 * @return boolean
-	 */
-	public function isNeutral($string)
-	{
-		return $this->decision($string) == self::NEUTRAL;
-	}
+    /**
+     * Tells if a sentence is positive.
+     *
+     * @param string $string The given sentence
+     *
+     * @return bool
+     */
+    public function isPositive($string)
+    {
+        return $this->decision($string) == self::POSITIVE;
+    }
+
+    /**
+     * Tells if a sentence is negative.
+     *
+     * @param string $string The given sentence
+     *
+     * @return bool
+     */
+    public function isNegative($string)
+    {
+        return $this->decision($string) == self::NEGATIVE;
+    }
+
+    /**
+     * Tells if a sentence is neutral.
+     *
+     * @param string $string The given sentence
+     *
+     * @return bool
+     */
+    public function isNeutral($string)
+    {
+        return $this->decision($string) == self::NEUTRAL;
+    }
 }
